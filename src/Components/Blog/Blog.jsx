0684f521815729f70a7f067c00e5d64a51e0479f
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-04-13 16:14:11 +0600

    css style added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,13 +1,32 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
     console.log(blog)
+    const {title, author, author_img, cover, reading_time, post_date, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-[700px] h-96' src={cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex gap-6 my-8'>
+                    <img src={author_img} alt="" />
+                    <div>
+                        <h4 className='text-2xl font-bold'>{author}</h4>
+                        <p className='text-[#11111199] font-semibold'>{post_date}</p>
+                    </div>
+                </div>
+                <div className='text-[#11111199] font-medium text-xl'>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl font-bold'>{title}</h2>
+            <p className='text-[#11111199] font-semibold text-xl'>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
-blog.propTypes.{
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 export default Blog;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import profile from '../../../../images/profile.png';
 
 const Header = () => {
     return (
-        <header className='flex justify-between items-center m-4 p-4 border-b-2'>
+        <header className='flex justify-between items-center m-4 p-4 border-b-2 max-w-6xl mx-auto'>
             <h1 className='font-bold text-4xl'>Knowledge Cafe</h1>
             <img src={profile} alt="" />
         </header>
